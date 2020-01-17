531c2f51d677946ea04110b504ccaff051f0dc50
JunLuo <junluo2018@outlook.com>
2020-01-17 13:18:34 -0800

    Initial Commit

---
+++ 5600assign1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  5600assign1
+//
+//  Created by Jun Luo on 1/17/20.
+//  Copyright © 2020 Jun Luo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
